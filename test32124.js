8c7640739c8d86d18554a935640a83a4ce19bc13
iAmTheRealJason <51766718+JasonGardner-code@users.noreply.github.com>
2024-03-21 05:28:15 -0700

    Update test32124.js

--- test32124.js
+++ test32124.js
@@ -27,4 +27,23 @@ const extra = {isJIT};
   let global8 = new WebAssembly.Global({value: 'anyfunc', mutable: true}, global0.value);
   let m2 = {fn1, fn2, global0, memory0, tag3: tag2, tag4: tag2, tag6: tag2};
   let m0 = {fn0, global1, global3, global4, global6: global3, tag2, tag7: tag2, tag10: tag2};
-  let m
+  let m1 = {global2, global5, global7, global8, tag5: tag2, tag8, tag9: tag2, debuggingHelper};
+  let importObject0 = /** @type {Imports2} */ ({extra, m0, m1, m2});
+  let i0 = await instantiate('repro_1289.wasm', importObject0);
+  let {fn3} =  (i0.instance.exports);
+  // log('calling fn3');
+  report('progress');
+  for (let k=0; k<24; k++) {
+    let zzz = fn3();
+    if (!(zzz instanceof Array)) { throw new Error('expected array but return value is '+zzz); }
+    if (zzz.length != 21) { throw new Error('expected array of length 21 but return value is '+zzz); }
+    let [r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20] = zzz;
+    r18?.toString();
+    debuggingHelper(r18);
+  }
+})().then(() => {
+  // log('after')
+  report('after');
+}).catch(e => {
+  report('after');
+})
